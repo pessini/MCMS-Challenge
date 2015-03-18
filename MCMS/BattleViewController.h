f5d08adb8dc062b724423ca4ca622dc8229bf623
Leandro Pessini <pessini@Leandros-MacBook-Air.local>
2015-03-18 00:06:08 -0300

    Add a new tableView to show the Battle

---
+++ MCMS/BattleViewController.h
@@ -0,0 +1,13 @@
+//
+//  BattleViewController.h
+//  MCMS
+//
+//  Created by Leandro Pessini on 3/17/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BattleViewController : UIViewController
+
+@end
---
+++ MCMS/BattleViewController.m
@@ -0,0 +1,22 @@
+//
+//  BattleViewController.m
+//  MCMS
+//
+//  Created by Leandro Pessini on 3/17/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import "BattleViewController.h"
+
+@interface BattleViewController ()
+
+@end
+
+@implementation BattleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
